a322e1a75983a451862e2f23160f3db36d4fa572
Sergei-Korneev <60899505+Sergei-Korneev@users.noreply.github.com>
2020-04-23 14:05:12 +0500

    Update entro.py

--- entro.py
+++ entro.py
@@ -1,12 +1,16 @@
 import string, getopt, sys, os, array, math, re, collections
 from sys import stdout
-hdd="\\\\.\\physicaldrive0"
-#hdd="C:\\Users\\sergei\\Desktop\\noname"
-block=96   #sectors that will be read in one step
-#sector=177301604
-sector=0  #first sector
+
+import matplotlib.pyplot as plt
+import numpy
 
 
+from random import seed
+from random import randint
+
+
+
+    
 if (sys.version_info.major != 3 and sys.version_info.minor != 8):
     print("This script is tested on Python 3.8!")
     print("You are using Python {}.{}.".format(sys.version_info.major, sys.version_info.minor))
@@ -24,7 +28,22 @@ def entropy(data):
 
         return e
         
-        
+
+
+#hdd="\\\\.\\physicaldrive0"
+hdd="C:\\Users\\sergei\\Desktop\\noname"
+block=24   #sectors that will be read in one step
+#sector=177301604
+sector=0  #first sector
+
+
+
+hl, = plt.plot([], [])
+plt.axis([0, os.stat(hdd).st_size, 0, 10])
+def update_line(hl, xd,yd):
+    hl.set_xdata(numpy.append(hl.get_xdata(), xd))
+    hl.set_ydata(numpy.append(hl.get_ydata(), yd))
+    plt.draw()        
         
 f = open(hdd, 'rb')
 
@@ -38,6 +57,7 @@ while read:
  f.seek(sector * 512)
  read = f.read(block*512) 
  s=entropy(read)
+ update_line(hl,sector*512,s)
  if s>7.95:
    print("Sector: {} | {}Mb | Entropy: {} <---".format(str(sector)+"-"+str(sector+block/512),str(round((sector*512)/1024/1024)) ,  str(round(s,3))))
    #break
@@ -47,3 +67,4 @@ while read:
  sector+=block
  sys.stdout.flush()
 f.close()
+plt.show()
