fb01bf1192e68087d64b50ae27756b12a4f90c63
Sergei-Korneev <60899505+Sergei-Korneev@users.noreply.github.com>
2020-04-23 14:56:20 +0500

    some changes
    add graph

--- entro.py
+++ entro.py
@@ -5,11 +5,6 @@ import matplotlib.pyplot as plt
 import numpy
 
 
-from random import seed
-from random import randint
-
-
-
     
 if (sys.version_info.major != 3 and sys.version_info.minor != 8):
     print("This script is tested on Python 3.8!")
@@ -30,41 +25,44 @@ def entropy(data):
         
 
 
-#hdd="\\\\.\\physicaldrive0"
-hdd="C:\\Users\\sergei\\Desktop\\noname"
-block=24   #sectors that will be read in one step
+hdd="\\\\.\\physicaldrive0"
+#hdd="C:\\Users\\sergei\\Desktop\\noname"
+block=512   #sectors that will be read in one step
 #sector=177301604
 sector=0  #first sector
-
-
+endsector=177916081
+start=sector
 
 hl, = plt.plot([], [])
-plt.axis([0, os.stat(hdd).st_size, 0, 10])
+plt.title('Entropy Graph') 
+plt.xlabel('x - Sectors') 
+plt.ylabel('y - Entropy') 
 def update_line(hl, xd,yd):
     hl.set_xdata(numpy.append(hl.get_xdata(), xd))
     hl.set_ydata(numpy.append(hl.get_ydata(), yd))
     plt.draw()        
+
         
 f = open(hdd, 'rb')
-
 f.seek(sector * 512)
-
-
 
 read = f.read(block)
 while read:
- 
+ if sector>endsector:
+  break
  f.seek(sector * 512)
  read = f.read(block*512) 
  s=entropy(read)
- update_line(hl,sector*512,s)
+ update_line(hl,round(sector)-start,s)
+ 
  if s>7.95:
-   print("Sector: {} | {}Mb | Entropy: {} <---".format(str(sector)+"-"+str(sector+block/512),str(round((sector*512)/1024/1024)) ,  str(round(s,3))))
+   print("Sector: {} | {}Mb | Entropy: {} <---".format(str(sector)+"-"+str(round(sector+block/512)),str(round((sector*512)/1024/1024)) ,  str(round(s,3))))
    #break
  else:
-   print("Sector: {} | {}Mb | Entropy: {} ".format(str(sector)+"-"+str(sector+block/512),str(round((sector*512)/1024/1024)) ,  str(round(s,3))), end="\r")
+   print("Sector: {} | {}Mb | Entropy: {} ".format(str(sector)+"-"+str(round(sector+block/512)),str(round((sector*512)/1024/1024)) ,  str(round(s,3))), end="\r")
    #print("Progress {:2.1%}".format(x / 10), end="\r")
  sector+=block
- sys.stdout.flush()
+
 f.close()
+plt.axis([0, endsector-start, 0, 10])
 plt.show()
